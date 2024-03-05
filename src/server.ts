33c542840a19293ebf2afa4153630281034c88ab
Albert <67117216+Albert8419@users.noreply.github.com>
2024-03-05 21:04:50 +0000

    1

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,10 @@ import weatherRoute from './routes/weatherRoutes.js';
 const app = express();
 // The port that the express server will listen on
 const PORT = 3000;
+
+import cors from "cors";
+app.use(cors());
+
 app.use(express.json());
 app.use('/api/weather', weatherRoute);
 // Start the express server
