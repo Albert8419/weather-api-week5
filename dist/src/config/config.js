71f95b9ebca980377e190f771f29221c19adb077
Albert <67117216+Albert8419@users.noreply.github.com>
2024-03-14 22:47:56 +0000

    1

--- dist/src/config/config.js
+++ dist/src/config/config.js
@@ -1,7 +1,7 @@
 import algosdk from "algosdk";
 const algodToken = "a".repeat(64);
 const server = "http://localhost";
-const port = "3000";
+const port = "4001";
 const mnemonic = "region cherry net oxygen ivory universe inside clog blade easily seven cargo scissors pen forget sustain front casual arrow fiscal ordinary local hurt above unable";
 export function getClient() {
     let client = new algosdk.Algodv2(algodToken, server, port);
