1db70aa76e2933fe3f8e1b6de3d340f365f35de4
Albert <67117216+Albert8419@users.noreply.github.com>
2024-03-14 23:16:07 +0000

    1

--- dist/src/config/config.js
+++ dist/src/config/config.js
@@ -1,8 +1,8 @@
 import algosdk from "algosdk";
 const algodToken = "a".repeat(64);
 const server = "http://localhost";
-const port = "4001";
-const mnemonic = "region cherry net oxygen ivory universe inside clog blade easily seven cargo scissors pen forget sustain front casual arrow fiscal ordinary local hurt above unable";
+const port = "3000";
+const mnemonic = "erode super only accident teach spatial morning nominee upon tomato panic laugh bargain baby tonight present leisure jewel priority creek clutch inside tiger abstract brain";
 export function getClient() {
     let client = new algosdk.Algodv2(algodToken, server, port);
     return client;
--- src/config/config.ts
+++ src/config/config.ts
@@ -2,7 +2,7 @@ import algosdk from "algosdk";
 
 const algodToken = "a".repeat(64);
 const server: string = "http://localhost";
-const port: string = "4001";
+const port: string = "3000";
 
 const mnemonic: string = "erode super only accident teach spatial morning nominee upon tomato panic laugh bargain baby tonight present leisure jewel priority creek clutch inside tiger abstract brain"
 
