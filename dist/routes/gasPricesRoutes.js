40df839e0c9dfe8082cd4314661af182a48f0a19
Albert <67117216+Albert8419@users.noreply.github.com>
2024-04-21 21:50:21 +0000

    1

--- dist/routes/gasPricesRoutes.js
+++ dist/routes/gasPricesRoutes.js
@@ -3,9 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const tslib_1 = require("tslib");
 const express_1 = tslib_1.__importDefault(require("express"));
 const gasPriceController_1 = require("../controllers/gasPriceController");
-const validators_1 = require("../middleware/validators"); // Make sure this path is correct
+const validators_1 = require("../middleware/validators"); // Ensure this path is correct and that validateCity is exported
+
 const router = express_1.default.Router();
+
 // Apply the validateCity middleware to the route to validate the 'city' parameter
-router.get('/gas-prices/:city', validators_1.validateCity, gasPriceController_1.getGasPrices);
+router.get('/:city', validators_1.validateCity, gasPriceController_1.getGasPrices);
+
 exports.default = router;
 //# sourceMappingURL=gasPricesRoutes.js.map
