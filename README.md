201c22a33d9645ef2d7be8df85d661d65dfc7910
Albert <67117216+Albert8419@users.noreply.github.com>
2024-04-21 06:49:02 +0000

    1

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Exported key for account DOYB6S5NRHDJNCGDVMDVNV6ZH5GGV5OYIX34XUOFAUZVXE2G7YSIKTX2S4: "erode super only accident teach spatial morning nominee upon tomato panic laugh bargain baby tonight present leisure jewel priority creek clutch inside tiger abstract brain"
+
 https://ubiquitous-fishstick-xxv9q5r9j543x9w-3000.app.github.dev/api/weather/london
 
-72HL6MVPXA57EZPQUMTJSRDJOBCEVFU7A3ZP6Y3ZPLYNOQRQ5MPO247FSQ: Exported key for account 72HL6MVPXA57EZPQUMTJSRDJOBCEVFU7A3ZP6Y3ZPLYNOQRQ5MPO247FSQ: "pigeon essence guitar sea spawn sheriff hold solid vote quote oblige hurdle entire senior situate pond boy pledge ladder weekend glare project nice abandon napkin"
+Exported key for account T5RKQ5WQ36WZMRBLYTITOTXK2E6E6A6HVCTLXBTDNQRBK4BVDYKNP6CP44: "fix insane term problem depth indicate luggage youth add enable happy struggle zoo main clutch tobacco swear one mirror mansion chaos exercise service abandon cherry"
--- dist/src/config/config.js
+++ dist/src/config/config.js
@@ -1,14 +1,14 @@
 import algosdk from "algosdk";
 // Assuming you're using a sandbox or private network, this token may be fine.
 // For a public Algorand network, you would need a valid token.
-const algodToken = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+const algodToken = "a".repeat(64);
 // This should be the address of your Algorand node.
 // If you're using the Algorand sandbox, localhost is correct, but the port should be 4001.
 const server = "http://localhost";
 const port = 4001; // Algorand's default port is 4001 for the algod process.
 // Your mnemonic to restore the account.
 // Ensure you protect this and don't expose it in your code in a production environment.
-const mnemonic = "pigeon essence guitar sea spawn sheriff hold solid vote quote oblige hurdle entire senior situate pond boy pledge ladder weekend glare project nice abandon napkin";
+const mnemonic = "ix insane term problem depth indicate luggage youth add enable happy struggle zoo main clutch tobacco swear one mirror mansion chaos exercise service abandon cherry";
 // Initialize Algorand client
 export function getClient() {
     return new algosdk.Algodv2(algodToken, server, port);
