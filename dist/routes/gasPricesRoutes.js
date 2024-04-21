68e6409172804f86285695627c216fc88ab3082f
Albert <67117216+Albert8419@users.noreply.github.com>
2024-04-21 17:34:08 +0000

    1

--- dist/routes/gasPricesRoutes.js
+++ dist/routes/gasPricesRoutes.js
@@ -2,17 +2,17 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const tslib_1 = require("tslib");
 const express_1 = tslib_1.__importDefault(require("express"));
-const gasPriceController_js_1 = require("../controllers/gasPriceController.js");
-const validators_js_1 = require("../middleware/validators.js");
+const gasPriceController_1 = require("../controllers/gasPriceController");
+const validators_1 = require("../middleware/validators");
 const router = express_1.default.Router();
 /**
  * Route for fetching gas prices with optional parameters validation.
  * Validates API key and gas price parameter.
  */
-router.get('/gas-prices', [validators_js_1.validateApiKey, validators_js_1.validateParameter], (req, res) => tslib_1.__awaiter(void 0, void 0, void 0, function* () {
+router.get('/gas-prices/:city', [validators_1.validateApiKey, validators_1.validateParameter], (req, res) => tslib_1.__awaiter(void 0, void 0, void 0, function* () {
     try {
         // Call the controller function to handle the request
-        yield (0, gasPriceController_js_1.getGasPrices)(req, res);
+        yield (0, gasPriceController_1.getGasPrices)(req, res);
     }
     catch (error) {
         console.error('Error in gas prices route:', error);
