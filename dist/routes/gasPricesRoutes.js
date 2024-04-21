e99cd327f2736275d5de4e05946476b58732cb57
Albert <67117216+Albert8419@users.noreply.github.com>
2024-04-21 15:36:46 +0000

    1

--- dist/routes/gasPricesRoutes.js
+++ dist/routes/gasPricesRoutes.js
@@ -3,9 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const tslib_1 = require("tslib");
 const express_1 = tslib_1.__importDefault(require("express"));
 const gasPriceController_js_1 = require("../controllers/gasPriceController.js");
-const validators_js_1 = require("../middleware/validators.js");
+const validators_1 = require("../middleware/validators");
 const router = express_1.default.Router();
 // Route for fetching gas prices with optional parameters validation
-router.get('/gas-prices', [validators_js_1.validateApiKey, validators_js_1.validateParameter], gasPriceController_js_1.getGasPrices);
+router.get('/gas-prices', [validators_1.validateApiKey, validators_1.validateParameter], gasPriceController_js_1.getGasPrices);
 exports.default = router;
 //# sourceMappingURL=gasPricesRoutes.js.map
